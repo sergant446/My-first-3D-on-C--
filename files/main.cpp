b063b5e50ba7843eb3a8b737062f715580a017d9
sergant446 <tsvu2009@mail.ru>
2024-02-03 15:37:49 +0300

    changes in main file

---
+++ files/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+  int screenWidth = 120;
+  int screenHeight = 30;
+  char* screen = new char[screenWidth * screenHeight + 1];
+  for (int i = 0; i < screenWidth; i++) {
+    for (int j = 0; j < screenHeight; j++) {
+
+    }
+  }
+  getchar();
+  return 0;
+}
