7f85b23f98115b0d82b131a044c5243716d244b1
sergant446 <tsvu2009@mail.ru>
2024-02-03 15:39:55 +0300

    changes in main

--- files/main.cpp
+++ files/main.cpp
@@ -5,6 +5,7 @@ int main() {
   int screenWidth = 120;
   int screenHeight = 30;
   char* screen = new char[screenWidth * screenHeight + 1];
+  screen[screenWidth * screenHeight] = '\0';
   for (int i = 0; i < screenWidth; i++) {
     for (int j = 0; j < screenHeight; j++) {
 
